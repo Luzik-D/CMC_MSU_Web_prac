ce14ada40f61540929f01b4c6649a1696b16fd4c
Dmitry L <dima.luzik@gmail.com>
2022-04-05 21:33:41 +0300

    prepare to DAO

---
+++ web_prac/src/main/java/ru/msu/cmc/web_prac/hibernate_db_cfg.java
@@ -0,0 +1,40 @@
+@Configuration
+@PropertySource("classpath:app.properties")
+public class HibernateDBCfg {
+    @Value("org.postgresql.Driver")
+    private String DB_DRIVER;
+    @Value("jdbc:postgresql://localhost/video_rental")
+    private String DB_URL;
+    @Value("postgres")
+    private String DB_USERNAME;
+    @Value("110921")
+    private String DB_PASSWORD;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(oraDataSource());
+        sessionFactory.setPackagesToScan("web_prac.tables");
+
+        Properties hibProperties = new Properties();
+        hibProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL11Dialect");
+        hibProperties.setProperty("connection_pool_size", "1");
+
+        sessionFactory.setHibernateProperties(hibProperties);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource oraDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+
+        return dataSource;
+    }
+}
