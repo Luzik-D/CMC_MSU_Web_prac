4f54859846c293231cf305729cd87a4e13aca4ae
Luzik-D <dima.luzik@gmail.com>
2022-04-22 03:53:05 +0300

    init new vers of web prac project

---
+++ src/main/java/ru/msu/cmc/web_prac/video_rental/VideoRentalApplication.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.web_prac.video_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalApplication.class, args);
+	}
+
+}
