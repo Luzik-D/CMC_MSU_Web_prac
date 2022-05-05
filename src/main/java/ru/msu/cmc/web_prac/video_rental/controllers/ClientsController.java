a815177359cce2d818cfc6c5dc4819ac8e1360a0
sunrise <dima.luzik@gmail.com>
2022-05-05 03:35:59 +0300

    more info about Film and Client according to page_description

--- src/main/java/ru/msu/cmc/web_prac/video_rental/controllers/ClientsController.java
+++ src/main/java/ru/msu/cmc/web_prac/video_rental/controllers/ClientsController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.msu.cmc.web_prac.video_rental.DAO.ClientDAO;
+import ru.msu.cmc.web_prac.video_rental.DAO.ClientHistoryRecordDAO;
+import ru.msu.cmc.web_prac.video_rental.DAO.CopyDAO;
 import ru.msu.cmc.web_prac.video_rental.DAO.Impl.ClientDAOImpl;
+import ru.msu.cmc.web_prac.video_rental.DAO.Impl.ClientHistoryRecordImpl;
+import ru.msu.cmc.web_prac.video_rental.DAO.Impl.CopyDAOImpl;
+import ru.msu.cmc.web_prac.video_rental.tables.ClientHistoryRecord;
+import ru.msu.cmc.web_prac.video_rental.tables.Copy;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/clients")
@@ -15,6 +23,9 @@ public class ClientsController {
 
     @Autowired
     private final ClientDAO clientDAO = new ClientDAOImpl();
+
+    @Autowired
+    private final ClientHistoryRecordDAO recordDAO = new ClientHistoryRecordImpl();
 
     @GetMapping()
     public String index(Model model) {
@@ -25,6 +36,17 @@ public class ClientsController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("client", clientDAO.getById(id));
+
+        //find all copies that client had from transaction history
+        List<ClientHistoryRecord> records = recordDAO.findRecord(id, null, null,
+                                null, null, null);
+
+        if(records == null) {
+            //client hasn't any record
+            System.out.println("client hasn't any record");
+        }
+        model.addAttribute("records", records);
+
         return "clients/show";
     }
 }
--- src/main/java/ru/msu/cmc/web_prac/video_rental/controllers/FilmsController.java
+++ src/main/java/ru/msu/cmc/web_prac/video_rental/controllers/FilmsController.java
@@ -6,8 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.msu.cmc.web_prac.video_rental.DAO.CopyDAO;
 import ru.msu.cmc.web_prac.video_rental.DAO.FilmDAO;
+import ru.msu.cmc.web_prac.video_rental.DAO.Impl.CopyDAOImpl;
 import ru.msu.cmc.web_prac.video_rental.DAO.Impl.FilmDAOImpl;
+import ru.msu.cmc.web_prac.video_rental.tables.Copy;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/films")
@@ -15,6 +20,9 @@ public class FilmsController {
 
     @Autowired
     private final FilmDAO filmDAO = new FilmDAOImpl();
+
+    @Autowired
+    private final CopyDAO copyDAO = new CopyDAOImpl();
 
     @GetMapping()
     public String index(Model model) {
@@ -25,6 +33,36 @@ public class FilmsController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("film", filmDAO.getById(id));
+
+        // get info about disks
+        List<Copy> disks = copyDAO.findCopy(id, "Диск", null, null);
+
+        //error if null
+        if(disks == null) {
+            //error
+        }
+
+        int disksAmount = disks.size();
+        int diskPrice = disks.get(0).getPrice();
+
+        //add to model
+        model.addAttribute("disksAmount", disksAmount);
+        model.addAttribute("diskPrice", diskPrice);
+
+        //get info about cassettes
+        List<Copy> cassettes = copyDAO.findCopy(id, "Кассета", null, null);
+
+        //error if null
+        if(cassettes == null) {
+            //error
+        }
+
+        int cassettesAmount = cassettes.size();
+        int cassettePrice = cassettes.get(0).getPrice();
+
+        //add to model
+        model.addAttribute("cassettesAmount", cassettesAmount);
+        model.addAttribute("cassettePrice", cassettePrice);
         return "films/show";
     }
 }
